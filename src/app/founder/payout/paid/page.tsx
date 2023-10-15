09cc016fa589efe0bb2076eb7d78bd8a0bb7ca30
Isaac Dyor <isaac@dyor.com>
2023-10-14 20:21:08 -0700

    ooohhhhh

--- src/app/founder/payout/paid/page.tsx
+++ src/app/founder/payout/paid/page.tsx
@@ -1,24 +1,61 @@
-"use client";
-import type { NextPage } from "next/types";
-
-import Link from "next/link";
+import type { GetStaticProps, NextPage } from "next";
 import Navbar from "../components/navbar";
 
-const NewInvestment: NextPage = () => {
+const DetailPage: NextPage = () => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-grow items-center justify-center  p-4">
-        <div className="w-full max-w-xl rounded border border-border  p-6">
-          <h1 className="mb-4 text-3xl font-semibold text-white">
-            Payout status
-          </h1>
-          <hr className="my-4 border-t-2 border-border" />
-          <div className="flex">
-            <div className="flex mr-2 h-5 border-2 bg-secondary border-border  rounded-full w-full">
-              <div className="bg-primary h-full w-1/5 rounded-l-full" />
+      <div className="flex justify-center pt-4 px-10">
+        <div className="flex w-full items-center justify-center rounded border-2 border-border text-white">
+          <div className="flex w-full flex-col">
+            <div className="flex flex-col border border-transparent border-b-border border-r-border  p-6">
+              <div className="flex items-center pb-6">
+                <h2 className="pr-4 text-2xl font-bold text-white ">Devlink</h2>
+                <p className="rounded-full bg-blue-400 px-3  text-center text-white">
+                  Payout
+                </p>
+              </div>
+              <p className="">Web Development</p>
             </div>
-            <p>$1,000/$5,000</p>
+            <div className="flex flex-col border border-transparent border-b-border border-r-border p-6">
+              <h2 className="pb-2 font-bold text-white">
+                Company Description:
+              </h2>
+              <p className="text-white">
+                An AI blog maker for your github commits.
+              </p>
+            </div>
+            <div className="flex flex-col border border-transparent border-b-border border-r-border p-6">
+              <h2 className="pb-2 font-bold text-white">Payout Status:</h2>
+              <div className="flex">
+                <div className="flex mr-2 h-5 border-2 bg-secondary border-border  rounded-full w-1/2">
+                  <div className="bg-primary h-full w-1/5 rounded-l-full" />
+                </div>
+                <p>$1,000/$5,000</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col border border-transparent border-b-border border-r-border p-6">
+              <h2 className="pb-2 font-bold text-white">Services Needed:</h2>
+              <p className="text-white">
+                I am looking for someone to make me a website
+              </p>
+            </div>
+            <div className="flex flex-col border border-transparent border-r-border p-6">
+              <div className="w-1/2">
+                <h2 className="pb-2 font-bold text-white">
+                  Recommended Skills:
+                </h2>
+                <div className="mt-2 flex flex-wrap ">
+                  <p className="mb-1 mr-1 rounded-2xl border border-gray-600 bg-neutral-700 p-1 px-2">
+                    React
+                  </p>
+                  <p className="mb-1 mr-1 rounded-2xl border border-gray-600 bg-neutral-700 p-1 px-2">
+                    Typescript
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -26,4 +63,4 @@ const NewInvestment: NextPage = () => {
   );
 };
 
-export default NewInvestment;
+export default DetailPage;
